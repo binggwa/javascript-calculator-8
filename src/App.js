07bc0d08afb5d136b7df68a45ab3a1bb48383ed0
binggwa <qudrhks317@gmail.com>
2025-10-17 22:37:06 +0900

    feat(App.js) : add function run
    - 요구사항에 맞게 라이브러리를 이용한 입출력 구현
    - AddCalculator를 이용해 입력받은 문자열을 계산
    - try-catch 블록을 통해 에러가 검출되면 에러 메시지와 함께 종료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Console } from "@woowacourse/mission-utils";
+import AddCalculator from './AddCalculator.js';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+
+      // 1. 문자열 입력
+      const input = Console.readLineAsync('덧셈할 문자열을 입력해주세요.');
+
+      // 2. 문자열 파싱 및 계산
+      const output = AddCalculator.add(input);
+
+      // 3. 출력
+      Console.print(`결과 : ${output}`);
+
+    } catch (error) {
+
+      // [ERROR] 로 시작하는 메시지 출력
+      Console.print(error.message);
+      
+    }
+  }
 }
 
 export default App;
