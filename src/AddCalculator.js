3e106f4c1e202a6bc08683614d4c25e1dcc342e9
binggwa <qudrhks317@gmail.com>
2025-10-17 22:16:14 +0900

    feat(AddCalculator) : add Error filtering, empty string return
    - 빈 문자열인 경우 0 반환하도록 추가 line 8
    - 파서를 통해 추출한 부분이 NaN일 경우 필터링 lint 20~22

--- src/AddCalculator.js
+++ src/AddCalculator.js
@@ -4,6 +4,9 @@ class AddCalculator {
 
     function add(text) {
 
+        // 빈 문자열이면 0 반환
+        if (text === '') return 0;
+
         // 입력받은 텍스트에서 구분자와 숫자문자열 구별
         const { separator, numStr } = StringParser.parseSeparatorAndNumber(text);
 
@@ -12,6 +15,11 @@ class AddCalculator {
 
         // 최종 구분자를 이용해 숫자 추출 및 정수로 변환
         const numbers = numStr.split(finalSeparator).map((n) => parseInt(n, 10));
+
+        // 추출된 문자가 숫자가 아닐 경우 필터링
+        if (numbers.filter((n) => Number.isNaN(n)).length > 0) {
+            throw new Error('[ERROR] 숫자가 아닌 값을 더할 수 없습니다!');
+        }
 
         // 추출한 정수를 누적해서 더하고 반환
         return numbers.reduce((a, b) => a + b, 0);
