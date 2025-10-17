2d376bb9323c55660a43534a7f82cefd6d6ab77b
binggwa <qudrhks317@gmail.com>
2025-10-17 21:38:27 +0900

    feat(AddCalculator) : add function add for calculating
    문자열을 입력받아 숫자만 더해주는 메소드
    - StringParser를 이용해 구분자와 numStr을 넘겨받는다
    - 커스텀 구분자 유무에 따른 최종 구분자를 결정한다
    - 최종 구분자를 이용해 numStr에서 숫자만 추출해서 정수로 변환한다
    - 변환한 정수를 누적해서 더하고 반환한다

---
+++ src/AddCalculator.js
@@ -0,0 +1,21 @@
+import StringParser from './StringParser.js';
+
+class AddCalculator {
+
+    function add(text) {
+
+        // 입력받은 텍스트에서 구분자와 숫자문자열 구별
+        const { separator, numStr } = StringParser.parseSeparatorAndNumber(text);
+
+        // 최종 구분자 결정
+        const finalSeparator = StringParser.makeFinalSeparator(separator);
+
+        // 최종 구분자를 이용해 숫자 추출 및 정수로 변환
+        const numbers = numStr.split(finalSeparator).map((n) => parseInt(n, 10));
+
+        // 추출한 정수를 누적해서 더하고 반환
+        return numbers.reduce((a, b) => a + b, 0);
+    }
+}
+
+export default AddCalculator
