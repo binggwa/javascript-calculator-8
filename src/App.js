d6091bbe4ee148a7640f0d43f3380a14d82336c5
binggwa <qudrhks317@gmail.com>
2025-10-20 10:29:41 +0900

    fix(App.js) : fix to throw an error
    - [ERROR]로 시작하는 테스트케이스 통과를 위해 throw하도록 변경 line 18
    - 실행 결과 예시에 일치하도록 readLineAsync의 문자열에 개행문자 추가
      line 8

--- src/App.js
+++ src/App.js
@@ -4,21 +4,18 @@ import AddCalculator from './AddCalculator.js';
 class App {
   async run() {
     try {
-
       // 1. 문자열 입력
-      const input = Console.readLineAsync('덧셈할 문자열을 입력해주세요.');
+      const input = await Console.readLineAsync('덧셈할 문자열을 입력해주세요.\n');
 
       // 2. 문자열 파싱 및 계산
       const output = AddCalculator.add(input);
 
       // 3. 출력
       Console.print(`결과 : ${output}`);
-
     } catch (error) {
-
-      // [ERROR] 로 시작하는 메시지 출력
+      // [ERROR] 로 시작하는 메시지 출력 후 에러 throw
       Console.print(error.message);
-
+      throw error;
     }
   }
 }
