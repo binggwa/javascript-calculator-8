5d50eab5b1635f5e4584c62a72507c8b7f2c9056
binggwa <qudrhks317@gmail.com>
2025-10-20 11:18:31 +0900

    refactor(AddCalculator) : delete NaN check
    - 정규식 조건 강화를 통해 추가적인 숫자 검증 필요성 제거
    - NaN 체크를 통해 숫자가 아닌 값을 계산할 수 없게 필터링하고 ERROR
      메시지를 던졌던 것을 ERROR 정규식 매칭 실패로 통합

--- src/AddCalculator.js
+++ src/AddCalculator.js
@@ -14,11 +14,6 @@ class AddCalculator {
         // 최종 구분자를 이용해 숫자 추출 및 정수로 변환
         const numbers = numStr.split(finalSeparator).map((n) => parseInt(n, 10));
 
-        // 추출된 문자가 숫자가 아닐 경우 필터링
-        if (numbers.filter((n) => Number.isNaN(n)).length > 0) {
-            throw new Error('[ERROR] 숫자가 아닌 값을 더할 수 없습니다!');
-        }
-
         // 추출한 정수를 누적해서 더하고 반환
         return numbers.reduce((a, b) => a + b, 0);
     }
