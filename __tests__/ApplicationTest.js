1cc58e4cd25a4b6491b950dd11228c5a846bcaeb
binggwa <qudrhks317@gmail.com>
2025-10-20 10:54:03 +0900

    test(ApplicationTest) : add test code
    - 기본 구분자 사용 테스트 추가
    - 커스텀 구분자 사용 테스트 변경
    - 커스텀, 기본 구분자 혼용 테스트 추가
    - 음수 문자 실패 테스트 변경
    - 구분자로 끝나는 경우 테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,5 @@
-import App from "../src/App.js";
-import { MissionUtils } from "@woowacourse/mission-utils";
+import App from '../src/App.js';
+import { MissionUtils } from '@woowacourse/mission-utils';
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -11,18 +11,33 @@ const mockQuestions = (inputs) => {
 };
 
 const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
   logSpy.mockClear();
   return logSpy;
 };
 
-describe("문자열 계산기", () => {
-  test("커스텀 구분자 사용", async () => {
-    const inputs = ["//;\\n1"];
+describe('문자열 계산기', () => {
+  test('기본 구분자 사용', async () => {
+    const inputs = ['1,2:3'];
     mockQuestions(inputs);
 
     const logSpy = getLogSpy();
-    const outputs = ["결과 : 1"];
+    const outputs = ['결과 : 6'];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  })
+
+  test('커스텀 구분자 사용', async () => {
+    const inputs = ['//;\\n1'];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ['결과 : 1'];
 
     const app = new App();
     await app.run();
@@ -32,12 +47,36 @@ describe("문자열 계산기", () => {
     });
   });
 
-  test("예외 테스트", async () => {
-    const inputs = ["-1,2,3"];
+  test('커스텀 구분자 및 기본 구분자 섞어서 사용', async () => {
+    const inputs = ['//ABC\\n1,3:5ABC7,9:11:13ABC15ABC17'];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ['결과 : 81'];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  })
+
+  test('음수 문자 실패', async () => {
+    const inputs = ['-1,2,3'];
     mockQuestions(inputs);
 
     const app = new App();
 
-    await expect(app.run()).rejects.toThrow("[ERROR]");
+    await expect(app.run()).rejects.toThrow(/^\[ERROR\]/);
   });
+
+  test('구분자로 끝나면 실패', async () => {
+    const inputs = ['1,2,3:'];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow(/^\[ERROR\]/);
+  })
 });
