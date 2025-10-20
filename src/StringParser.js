6ca121a934989e69d03e7309c1094c08d6e7af3f
binggwa <qudrhks317@gmail.com>
2025-10-20 10:35:43 +0900

    fix(StringParser) : fix REGEX to pass test cases
    - REGEX는 고정된 상수로 취급해 SNAKE_CASE로 변경
    - 그룹 2를 기존 .+에서 \d+ (숫자)와 (?:(?:[:,]|\1)\d+)* 반복으로 변경
    - 커스텀 구분자 레퍼런스 \1을 이용해 숫자들이 콤마, 콜론, 커스텀
      구분자로만 이어지게 변경
    - 그룹 3은 커스텀 구분자가 없을 때, 기본 구분자와 숫자로만 이어지게
      변경
    - 음수 테스트 케이스 통과 가능
    - 테크코스 페이지의 기능요구사항인 "\n"과 테스트케이스의 "\\n" 중
      테스트케이스 통과 선택

--- src/StringParser.js
+++ src/StringParser.js
@@ -1,9 +1,14 @@
 class StringParser {
 
     // 커스텀 문자열 구분을 위한 정규식
-    static regex = /^(?:\/\/(.+)\n(.+)|(.+))$/;
+    static REGEX = /^(?:\/\/(.+)\\n((?:\d+(?:(?:[:,]|\1)\d+)*))|((?:\d+(?:(?:[:,])\d+)*)))$/;
 
-    // 입력받은 text를 파싱하는 기능
+    /**
+     * 입력받은 문자열을 정규식으로 필터링하여, 구분자와 덧셈 문자열로 반환한다.
+     * @param {string} text 정규식으로 검사할 문자열
+     * @throws 정규식 매칭에 실패했을 때
+     * @returns {{ separator: string, numStr: string }} 구분자 및 덧셈 문자열
+     */
     static parseSeparatorAndNumber(text) {
 
         // 입력값이 문자열이 아닐 경우 필터링
@@ -12,7 +17,7 @@ class StringParser {
         }
 
         // match 메소드를 이용해 커스텀 구분자 존재여부 확인
-        const match = text.match(regex);
+        const match = text.match(StringParser.REGEX);
 
         // 정규식에 맞지 않는 문자열이 들어왔을 경우 필터링
         if (!match) {
@@ -34,7 +39,11 @@ class StringParser {
         };
     }
 
-    // 커스텀 구분자를 포함한 최종 구분자 생성
+    /**
+     * 커스텀 구분자가 있는 경우, 커스텀 구분자를 포함한 최종 구분자를 반환하고, 없는 경우 기본 구분자로 구성된 정규식을 반환한다.
+     * @param {string | null} separator 커스텀 구분자, 커스텀이 없다면 null
+     * @returns {RegExp} 최종 구분자로 사용할 정규식 객체
+     */
     static makeFinalSeparator(separator) {
         if (!separator) return /[:,]/;
         return new RegExp(`(?:${[':',',',separator].join('|')})`);
